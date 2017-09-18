bd07615d53bef0033dd32fb188ab64ef6e33e39e
Michael Klein <miketheklein@gmail.com>
2017-09-18 11:26:45 -0700

    basic splashpage and navbar working

--- src/server.js
+++ src/server.js
@@ -4,11 +4,16 @@ const server = express();
 const router = express.Router();
 const port = process.env.PORT || 3000;
 
+server.set('views', './src/views');
+server.set('view engine', 'ejs');
+server.use(express.static('./src/public'));
+
 router.get('/', (request, response) => {
-  response.send('hello raom');
+  response.render('home', { user: 'hi' });
 });
 
 server.use('/', router);
+
 
 server.listen(3000, (err) => {
   console.log(`<3333333 running server on port ${port} <3333333`);

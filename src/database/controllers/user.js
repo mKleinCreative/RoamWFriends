27f759a0cada24cd49a4725a650c2096ff95f04d
jnware7 <jnware7@gmail.com>
2017-09-19 10:28:24 -0700

    Added user validation and added encryption

--- src/database/controllers/user.js
+++ src/database/controllers/user.js
@@ -1,13 +1,29 @@
-const { db } = require('./index');
+const { db, bcrypt } = require('./index');
 
-const create = (email, password) => {
-  return db.one(`
-    INSERT INTO 
-      users (email, password)
-    VALUES($1, $2)
-    RETURNING *
-    `, [email,
-      password])
+const saltRounds = 12;
+
+const create = (email,password) => {
+  return bcrypt.hash(password, saltRounds)
+    .then((hash) => {
+      return db.one(`
+      INSERT INTO
+        users (email, password)
+      VALUES($1, $2)
+      RETURNING *
+      `, [email, hash]);
+    });
 };
 
-module.exports = { create };
+const getUserByEmail = (email) => {
+  return db.oneOrNone(`
+    SELECT
+     email
+    FROM users
+    WHERE
+     email=$1
+    `,[email])
+}
+
+module.exports = { create,
+                   getUserByEmail,
+                 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -14,16 +14,27 @@ router.post('/signup', (request, response) => {
   const { email, password, confirmPassword } = request.body;
   const validatePassword = password === confirmPassword;
 
+
   if (!validatePassword) {
-    return response.render('signup', { user: '', message: "Passwords don't match" });
+    return response.render('signup', { user: '', message: 'Passwords don\'t match' });
   }
-  user.create(email, password)
-    .then((newUser) => {
-      response.send({ newUser });
-    })
-    .catch((error) => {
-      console.log('---===error.message===---', error.message);
-    });
+  user.getUserByEmail(email)
+   .then((existingUser)=>{
+
+     if (existingUser) {
+       return response.render('signup', { user: '', message: 'Accout with this email exists' });
+     }else if (!existingUser){
+       user.create(email, password)
+         .then((newUser) => {
+           response.send({ newUser });
+         })
+         .catch((error) => {
+           console.log('---===error.message===---', error.message);
+         });
+     }
+   }).catch((error) => {
+       console.log('---===error.message===---', error.message);
+     });
 });
 
 router.get('/login', (request, response) => {
